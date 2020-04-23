9b44b71c779b3d7d05edcbbbd1760c21c85709be
Alex Malz <aimalz@nyu.edu>
2020-04-23 10:43:33 -0400

    defining data type

--- barber/cutter.py
+++ barber/cutter.py
@@ -1,18 +1,21 @@
 import numpy as np
+import numpy.random as npr
 import scipy.optimize as spo
 
 TreePars = namedtuple(TreePars', ['cut_vals', 'tree_ids'])
 
-def get_cuts(galaxies, nbins=3, tree_pars_ivals=None):
+def get_cuts(galaxies, ivals=None, nbins=3):
     """
     Parameters
     ----------
     galaxies: numpy.ndarray, float
-        features (magnitudes and/or colors)
+        observables (magnitudes and/or colors) to serve as features for set of galaxies
+    ivals: namedtuple, numpy.ndarray, float and int
+        initial values for decision tree parameters
 
     Returns
     -------
-    assignments:
+    assignments: numpy.ndarray, int
         bin assignment for each galaxy
 
     Notes
@@ -21,19 +24,21 @@ def get_cuts(galaxies, nbins=3, tree_pars_ivals=None):
     eval_metric is from tomo_challenge
     """
     (ngals, nfeat) = np.shape(galaxies)
-    if ivals is None:
+
+    if tree_pars_ivals is None:
         cut_ivals = np.quantile(galaxies, np.linspace(0., 1., nbins), axis=1)
         assert(len(np.flatten(ivals)) == nbins**nfeat)
 
         # need structure and way of making dumb version of these
-        tree_ids = None
+        tree_ids = npr.random_integers(0, nbins, nbins**nfeat)
         assert(len(np.unique(tree_ids)) == nbins)
+        tree_ids.reshape((nfeat, nbins))
 
         ivals = TreePars(cut_ivals, tree_ids)
 
-    tree_pars = spo.optimize(eval_metric, ivals, args=galaxies)
+    tree_pars = spo.minimize(eval_metric, ivals, args=galaxies)
 
-    assignments = sort_gals(galaxies, tree_pars.res)
+    assignments = sort_gals(galaxies, tree_pars.x)
 
     return(assignments)
 
