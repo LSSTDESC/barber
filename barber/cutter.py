1aa56e052803de11b083fc77ceeaa921ea4b2447
Alex Malz <aimalz@nyu.edu>
2020-04-23 10:16:03 -0400

    dumbest skeleton pseudocode

---
+++ barber/cutter.py
@@ -0,0 +1,25 @@
+import scipy.optimize as spo
+
+def get_cuts(galaxies, nbins=3):
+    """
+    Parameters
+    ----------
+    galaxies: numpy.ndarray, float
+        features (magnitudes and/or colors)
+
+    Returns
+    -------
+    bin_assignments:
+        bin assignment for each galaxy
+    """
+    pass
+
+
+def sort_gals(galaxies, cuts):
+    """
+    [calls to bisect]
+    """
+    pass
+
+
+# then call metric from tomo_challenge
