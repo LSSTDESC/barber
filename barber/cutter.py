81f999336b5b86864d8ac834cfbbb6d09b9198ab
Alex Malz <aimalz@nyu.edu>
2020-04-23 10:35:30 -0400

    filling in optimizer step

--- barber/cutter.py
+++ barber/cutter.py
@@ -1,7 +1,9 @@
 import numpy as np
 import scipy.optimize as spo
 
-def get_cuts(galaxies, nbins=3, ivals=None):
+TreePars = namedtuple(TreePars', ['cut_vals', 'tree_ids'])
+
+def get_cuts(galaxies, nbins=3, tree_pars_ivals=None):
     """
     Parameters
     ----------
@@ -18,10 +20,21 @@ def get_cuts(galaxies, nbins=3, ivals=None):
     sort_gals does the heavy lifting
     eval_metric is from tomo_challenge
     """
+    (ngals, nfeat) = np.shape(galaxies)
     if ivals is None:
-        ivals = np.mean(galaxies, axis=1)
-    cuts = spo.optimize(eval_metric, ivals, args=galaxies)
-    assignments = sort_gals(galaxies, cuts)
+        cut_ivals = np.quantile(galaxies, np.linspace(0., 1., nbins), axis=1)
+        assert(len(np.flatten(ivals)) == nbins**nfeat)
+
+        # need structure and way of making dumb version of these
+        tree_ids = None
+        assert(len(np.unique(tree_ids)) == nbins)
+
+        ivals = TreePars(cut_ivals, tree_ids)
+
+    tree_pars = spo.optimize(eval_metric, ivals, args=galaxies)
+
+    assignments = sort_gals(galaxies, tree_pars.res)
+
     return(assignments)
 
 
@@ -32,10 +45,10 @@ def sort_gals(galaxies, cuts):
     pass
 
 
-def eval_metric(cuts, galaxies):
+def eval_metric(galaxies, tree_pars):
     """
 
     """
-    assignments = sort_gals(galaxies, cuts)
+    assignments = sort_gals(galaxies, tree_pars)
     metval = tomo_challenge.metric(assignments)
     return metval
