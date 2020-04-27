5c668ff29f7712bd030546c7d1a6bc73a7715bb4
Joe Zuntz <joezuntz@googlemail.com>
2020-04-27 15:38:30 +0100

    superclass variant - untested, moving to nersc now to test

---
+++ barber/main.py
@@ -0,0 +1,20 @@
+from .data import load
+from .tree import DecisionTreeMethod
+
+
+def main():
+    training_data, training_z = load('training', thin=100)
+    validation_data, validation_z = load('validation')
+    nbin = 4
+    T = DecisionTreeMethod(nbin,
+                 training_data, training_z, 
+                 validation_data, validation_z)
+    z_edges, score = T.optimize()
+
+    z_edges = ', '.join(f'{z:.2f}' for z in z_edges)
+    print(f"nbin: {nbin} score: {score}")
+    print(f"z: {z_edges}")
+
+
+if __name__ == '__main__':
+    main()
